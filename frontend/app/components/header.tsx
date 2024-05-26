f4d9198e2e2e384007288cad948a6dfcdb226e18
jraleman <jraleman@bendiburg.com>
2024-05-25 20:37:35 -0400

    fix: header styles bottom overlay, background gradient

--- frontend/app/components/header.tsx
+++ frontend/app/components/header.tsx
@@ -1,16 +1,20 @@
 import Image from "next/image";
 
-export default function Header() {
+export type Props = {
+  title: string;
+}
+
+function Header({ title }: Props) {
   return (
-    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-      <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-        (L)lama(I)ndex as a (R)hythmic (I)nsightful (C)reative (S)emantic (S)ystem
-      </p>
+    <div className="z-10 max-w-5xl w-full items-center justify-between lg:flex">
+      <h2 className="text-xl font-medium fixed left-0 top-0 flex w-full justify-center pb-6 pt-8 lg:static lg:w-auto lg:rounded-xl lg:p-4 p-6">
+        {title}
+      </h2>
       <div className="flex">
       </div>
-      <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+      <div className="fixed bottom-0 left-0 flex margin-bottom-10 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
         <a
-          href="https://www.llamaindex.ai/"
+          href="https://www.raisga.com/"
           className="flex items-center justify-center font-nunito text-lg font-bold gap-2"
         >
           <span>LI-RICSS</span>
@@ -27,3 +31,5 @@ export default function Header() {
     </div>
   );
 }
+
+export default Header;
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,14 @@
 import Header from "@/app/components/header";
 import ChatSection from "./components/chat/chat-section";
 
-export default function Home() {
+function Home() {
+  const headerTitle = '(L)lama(I)ndex as a (R)hythmic (I)nsightful (C)reative (S)emantic (S)ystem';
   return (
     <main className="flex min-h-screen flex-col items-center gap-10 p-24 background-gradient">
-      <Header />
+      <Header title={headerTitle} />
       <ChatSection />
     </main>
   );
 }
+
+export default Home;
