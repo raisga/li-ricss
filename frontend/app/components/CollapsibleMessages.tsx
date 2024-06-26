58241b5227dc72dd0a812a146612e1c070789062
jraleman <jraleman@bendiburg.com>
2024-06-26 14:45:45 -0400

    feat: replaces collapsible with accordion component

--- frontend/app/components/CollapsibleMessages.tsx
+++ frontend/app/components/CollapsibleMessages.tsx
@@ -1,27 +1,34 @@
 import React from 'react';
-import Collapsible from 'react-collapsible';
+import * as Accordion from '@radix-ui/react-accordion';
 import { Message } from 'ai/react';
 import MarkdownParser from './MarkdownParser';
 
-// TODO: Replace with Accordion component in the future
-// https://www.radix-ui.com/primitives/docs/components/accordion
 const CollapsibleMessages = ({ messages }: { messages: Message[] }) => {
   const title = 'Past versions';
   return (
     <div>
       <h3 style={{ fontSize: '1.5rem', color: '#222', marginBottom: '1rem' }}>{title}</h3>
-      {messages.map(({ id, content }) => (
-        <Collapsible 
-          key={id} 
-          trigger={id} 
-          transitionTime={100} 
-          style={{ marginBottom: '1rem', backgroundColor: '#f5f5f5', margin: '0.5rem' }}
-        >
-          <div style={{ backgroundColor: '#f5f5f5', margin: '0.5rem' }}>
-            <MarkdownParser content={content} />
-          </div>
-        </Collapsible>
-      ))}
+      <Accordion.Root
+        className="bg-mauve6 w-[300px] rounded-md shadow-[0_2px_10px] shadow-black/5"
+        type="single"
+        defaultValue="item-1"
+        collapsible
+      >
+        {messages.map(({ id, content }) => (
+          <Accordion.Item value={id}>
+            <Accordion.Header>
+              <Accordion.Trigger>
+                <p>{id}</p>
+              </Accordion.Trigger>
+            </Accordion.Header>
+            <Accordion.Content>
+            <div style={{ backgroundColor: '#f5f5f5', margin: '0.5rem' }}>
+                      <MarkdownParser content={content} />
+                    </div>
+              </Accordion.Content>
+          </Accordion.Item>
+        ))}
+      </Accordion.Root>
     </div>
   );
 };
